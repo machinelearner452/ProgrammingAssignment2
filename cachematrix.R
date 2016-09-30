de1ffd239cce90de6b2fb49a1221ade25a5ff535
machinelearner452 <machinelearner452@users.noreply.github.com>
2016-09-29 21:42:08 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions, makeVector and cachemean.
+## The function designed to aid understand Lexical Scoping and how it works in R
 
-## Write a short comment describing this function
+## This function call creates vectors based on numeric input.
+## Matric is set to NULL, using the assignment operator: <-, 
+## superassignment operator: <<- to have the mean assigned at a higher level environment then the calling function
+
 
 makeVector <- function(x = numeric()) {
         m <- NULL
@@ -18,7 +21,9 @@ makeVector <- function(x = numeric()) {
 }
 
 
-## Write a short comment describing this function
+## cacheman is a function that does an element by element mean calculation of the "m" matric.
+## If the "m" matrices is not null, it used the cached store value in a higher level environment in place of have the calculation performed every time the function is called.
+##  This matric checking method comes into aid when doing machine learning applications.
 
 
 cachemean <- function(x, ...) {
